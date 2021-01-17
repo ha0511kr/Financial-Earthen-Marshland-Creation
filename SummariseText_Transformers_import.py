c002fe0c21462f569d40e14a0f715a029e60e95f
nguyentuthanhdn <44814087+nguyentuthanhdn@users.noreply.github.com>
2021-01-17 11:11:31 -0700

    Update SummariseText_Transformers_import.py

--- SummariseText_Transformers_import.py
+++ SummariseText_Transformers_import.py
@@ -1,6 +1,11 @@
 from transformers import pipeline
-summarizer = pipeline("summarization")
+class SummariseText:
+  def __init__(self):
+    self.summarizer = pipeline("summarization")
+    self.text = ""
 
-textInput = input()
-
-print(summarizer(textInput, max_length=450, min_length=30, do_sample=False)[0]["summary_text"])
+  def setInput(self, textInput):
+    self.text = textInput
+  
+  def getSummary(self):
+    return self.summarizer(textInput, max_length=450, min_length=30, do_sample=False)[0]["summary_text"]
