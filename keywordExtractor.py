e231b1998b0df8f2672c569f29a1400e5d568627
nguyentuthanhdn <44814087+nguyentuthanhdn@users.noreply.github.com>
2021-01-17 11:04:24 -0700

    keyBERT extractor

---
+++ keywordExtractor.py
@@ -0,0 +1,12 @@
+from keybert import KeyBERT
+class Keyword:
+    def __init__(self):
+        self.model = KeyBERT('xlm-r-distilroberta-base-paraphrase-v1')
+        self.textInput = ""
+    def setInput(self, textInput):
+        self.textInput = textInput
+    def getKeyword(self):
+        keyword = self.model.extract_keywords(doc, keyphrase_ngram_range=(1, 2), stop_words=None)
+        return keyword
+
+
